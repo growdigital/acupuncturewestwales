448b3fca077a6dbabc4e58a06d0833e972e86ce4
Jake Rayson <hi@git.grwd.uk>
2023-01-28 22:50:58 +0000

    delete extraneous files

--- assets/js/call.js
+++
@@ -1,7 +0,0 @@
-var ein = document.querySelector(".plain");
-var eout = document.querySelector(".markdown");
-function update() {
-  eout.innerHTML = markdown(ein.value);
-}
-ein.addEventListener("input", update);
-update();
